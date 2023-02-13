f22ad5604508d9193f63e49babc09bbfb182ca48
PAVAN KUMAR RAAVI <121175762+PAVAN-RAAVI@users.noreply.github.com>
2023-02-13 19:08:42 +0530

    Day 14:Design of SR_FLIPFLOP

---
+++ SR FLIPFLOP/SR_FLIPFLOP.v
@@ -0,0 +1,14 @@
+
+////////////////////GATE LEVEL //////////////////////////////
+module SR_FLIPFLOP(S,R,Q,Q_bar);
+input S,R;
+output Q,Q_bar;
+wire nand1_out; // output of nand1 
+wire nand2_out; // output of nand2 
+
+nand (nand1_out,S); 
+nand (nand2_out,R); 
+nand (Q,nand1_out,Q_bar);
+nand (Q_bar,nand2_out,Q);
+
+endmodule
